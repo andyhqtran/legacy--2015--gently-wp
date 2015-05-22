9e6a59e82ec522ed6bae8a9eeaf413518c70a995
Andy Tran <andyhqtran@gmail.com>
2015-05-22 15:30:03 -0700

    Added in WooCommerce Support.

--- functions.php
+++ functions.php
@@ -32,6 +32,8 @@ if (!function_exists('gently_setup')):
 			'default-color' => 'ffffff',
 			'default-image' => '',
 		)));
+
+		add_theme_support('woocommerce');
 	}
 endif;
 add_action('after_setup_theme', 'gently_setup');
@@ -101,12 +103,6 @@ remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrap
 
 add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
 add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
-
-add_action('after_setup_theme', 'woocommerce_support');
-
-function gently_woocommerce_support() {
-	add_theme_support('woocommerce');
-}
 
 function gently_woocommerce_wrapper_start() {
 	echo '
--- page.php
+++ page.php
@@ -1,23 +1,10 @@
 <?php get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'content', 'page' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
-			<?php endwhile; // end of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		<?php if (have_posts()):
+	while (have_posts()): the_post();
+		the_content();
+	endwhile;
+endif;?>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
