ee51a255cb7a191cc11c41d2c322de27df959d53
Andy Tran <andyhqtran@gmail.com>
2015-05-22 03:25:55 -0700

    Added in WooCommerce Support.

--- functions.php
+++ functions.php
@@ -87,6 +87,35 @@ function gently_add_to_author_contact($contactmethods) {
 }
 add_filter('user_contactmethods', 'gently_add_to_author_contact', 10, 1);
 
+remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+add_action('after_setup_theme', 'woocommerce_support');
+function woocommerce_support() {
+	add_theme_support('woocommerce');
+}
+
+function my_theme_wrapper_start() {
+	echo '
+		<section id="woocommerce">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+	';
+}
+
+function my_theme_wrapper_end() {
+	echo '
+					</div>
+				</div>
+			</div>
+		</section>
+	';
+}
+
 require get_template_directory() . '/inc/template-tags.php';
 require get_template_directory() . '/inc/extras.php';
 require get_template_directory() . '/inc/customizer.php';
--- page-template-home.php
+++ page-template-home.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Home Page
+	Template Name: Home Page
  */
 get_header();?>
 
@@ -13,6 +13,99 @@ get_header();?>
 				</div>
 			</div>
 		</div>
+		<?php
+			$i = 1;
+			$args = array(
+				'post_type' => 'product',
+				'meta_key' => '_featured',
+				'meta_value' => 'yes',
+				'posts_per_page' => 3,
+				'order' => 'ASC',
+				'orderby' => 'ID',
+			);
+
+			$featured_query = new WP_Query($args);
+				if ($featured_query->have_posts()):
+					while ($featured_query->have_posts()):
+						$featured_query->the_post();
+						$product = get_product($featured_query->post->ID);
+		?>
+		<div class='row'>
+	  		<div class='col-md-12'>
+
+		        <?php if (is_int($i / 2)) { ?>
+				<div class='featured-project'>
+					<div class='row'>
+						<div class='col-md-7'>
+							<div class='featured-project-thumbnail'>
+								<div class='browser'>
+									<div class='screen'>
+										<?php the_post_thumbnail('large', array('class' => 'img-responsive'));?>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class='col-md-5'>
+							<h3 class='project-title'><?php the_title(); ?></h3>
+							<?php
+					            $size = sizeof( get_the_terms( $post->ID, 'product_cat' ) );
+					            echo $product->get_categories( ', ', '<div class="project-meta">' . _n( '', '', $size, 'woocommerce' ) . ' ', '</div>' );
+				    		?>
+							<div class='project-description'>
+								<?php
+									$content = get_the_content();
+										if (strlen($content) > 250) {
+											echo substr($content, 0, 250) . "...";
+										} else {
+												the_content();
+										}
+								?>
+							</div>
+						  	<a class='btn project-btn' href='<?php the_permalink();?>'>View Project</a>
+						</div>
+					</div>
+				</div>
+		        <?php } else { ?>
+				<div class='featured-project'>
+					<div class='row'>
+						<div class='col-md-7 col-md-push-5'>
+							<div class='featured-project-thumbnail'>
+								<div class='macbook'>
+									<div class='screen'>
+										<?php the_post_thumbnail('large', array('class' => 'img-responsive'));?>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class='col-md-5 col-md-pull-7'>
+							<h3 class='project-title'><?php the_title(); ?></h3>
+							<?php
+					            $size = sizeof( get_the_terms( $post->ID, 'product_cat' ) );
+					            echo $product->get_categories( ', ', '<div class="project-meta">' . _n( '', '', $size, 'woocommerce' ) . ' ', '.</div>' );
+				    		?>
+							<div class='project-description'>
+								<?php
+									$content = get_the_content();
+										if (strlen($content) > 250) {
+											echo substr($content, 0, 250) . "...";
+										} else {
+												the_content();
+										}
+								?>
+							</div>
+						  	<a class='btn project-btn' href='<?php the_permalink();?>'>View Project</a>
+						</div>
+					</div>
+				</div>
+				<?php } ?>
+			</div>
+		</div>
+			<?php
+				$i++;
+				endwhile;
+			endif;
+			wp_reset_query();
+			?>
 	</div>
 </section>
 
